04f3a141e2e1e94685282171280d3f4b4b8a178b
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-01-07 15:50:07 +0100

    (Add) Basic getters and setters for player + begin simple gamestate system

--- Game/Game.py
+++ Game/Game.py
@@ -1,15 +1,30 @@
+from enum import Enum
+
 from Player import Player
+
+class GameState(Enum):
+    MAINMENU = 0,
+    PLAYING = 1,
+    LOST = 2,
 
 class Game:
     Ongoing = True
+    State = GameState.MAINMENU
 
     def __init__(self):
-        player = Player("Name", 100, 2)
+        while self.Ongoing:
+            if self.State == GameState.MAINMENU:
+                player_input = input("Available Commands: \nPlay - Start the game \nQuit - Quit the game \n\nPlease enter your command: ").casefold()
 
-        while Game.Ongoing:
-            player_input = input("Enter a command:")
+                if player_input == "exit".casefold() or player_input == "quit".casefold():
+                    self.Ongoing = False
+                if player_input == "start".casefold() or player_input == "play".casefold():
+                    self.State = GameState.PLAYING
 
-            if player_input == "exit":
-                game.Ongoing = False
+            if self.State == GameState.PLAYING:
+                player_input = input("Please enter your name: ")
+                player = Player(player_input, 100, 2)
+
+                print("Your name is " + player.name)
 
 game = Game()
--- Game/Player.py
+++ Game/Player.py
@@ -1,7 +1,56 @@
+import random
+
+def clamp(n, smallest, largest):
+    return max(smallest, min(n, largest))
+
 class Player:
     isAlive = True
 
-    def __init__(self, name: str, health: int, strength: int):
-        self.name = name
-        self.health = health
-        self.strength = strength
+    def __init__(self, name: str):
+        self._name = name
+        self._max_health = random.randint(50, 120)
+        self._health = self._max_health
+        self._damage = 10
+        self._damage_protection = random.uniform(0, 2)
+        self._damage_multiplier = random.uniform(0.8, 2)
+
+    def take_damage(self, damage: int):
+        self._health -= damage
+
+    def attack(self, target):
+        target.health -= self._damage * self._damage_multiplier
+
+    ###########
+    # Getters #
+    ###########
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def health(self):
+        return self._health
+
+    @property
+    def damage_multiplier(self):
+        return self._damage_multiplier
+
+    @property
+    def damage(self):
+        return self._damage
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @health.setter
+    def health(self, value: int):
+        self._health = clamp(self._health + value, 0, self._max_health)
+
+    @damage_multiplier.setter
+    def damage_multiplier(self, value: int):
+        self._damage_multiplier = value
+
+    @damage.setter
+    def damage(self, value: int):
+        self._damage = value
