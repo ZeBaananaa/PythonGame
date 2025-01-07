d4f3f37dd6a72e7a7112fd039a51c381af0965fc
Ethan Ruhlmann <e.ruhlmann@student.isartdigital.com>
2025-01-07 15:52:00 +0100

    (Edit) Rename variable

--- Game/Player.py
+++ Game/Player.py
@@ -11,7 +11,7 @@ class Player:
         self._max_health = random.randint(50, 120)
         self._health = self._max_health
         self._damage = 10
-        self._damage_protection = random.uniform(0, 2)
+        self._shield = random.uniform(0, 2)
         self._damage_multiplier = random.uniform(0.8, 2)
 
     def take_damage(self, damage: int):
@@ -39,6 +39,10 @@ class Player:
     def damage(self):
         return self._damage
 
+    @property
+    def shield(self):
+        return self._shield
+
     @name.setter
     def name(self, value: str):
         self._name = value
@@ -54,3 +58,7 @@ class Player:
     @damage.setter
     def damage(self, value: int):
         self._damage = value
+
+    @shield.setter
+    def shield(self, value: int):
+        self._shield = value
